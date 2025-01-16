9cde9fb48f01fbddba1ff37c465a796871d5e30d
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2025-01-16 22:04:10 +0530

    initial commit

---
+++ TheLastRefugee/Assets/FurnishedCabin/Scripts/Player/LeftMainDoor.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class LeftDoorController : MonoBehaviour
+{
+    private bool isOpen = false; // Tracks the door's state (open/closed)
+    private float closedRotation = 0f; // Y rotation when the door is closed
+    private float openRotation = -90f; // Y rotation when the door is open
+
+    void OnMouseDown()
+    {
+        ToggleDoor(); // Trigger when the object is clicked
+    }
+
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.E)) // Trigger when E key is pressed
+    //     {
+    //         ToggleDoor();
+    //     }
+    // }
+
+    private void ToggleDoor()
+    {
+        isOpen = !isOpen; // Toggle the state
+        float targetRotationY = isOpen ? openRotation : closedRotation;
+
+        // Update the rotation of the door
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, targetRotationY, transform.rotation.eulerAngles.z);
+
+        Debug.Log("Door toggled. isOpen = " + isOpen);
+    }
+}
+
